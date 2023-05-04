fb64deac61df5025e8059a4b0a2fc6dde1acd103
jon <jon@pathbreaking.net>
2023-05-03 22:25:31 -0400

    adding mastodon bot v1

--- assets/ifdictionary.js
+++ assets/ifdictionary.js
@@ -1,25 +1,45 @@
 var arrayifredcard = new Array(
+  "Dual-use surveillance technology exports are being legitimized by __",
   "USG funded __",
+  "We need rapid-response capabilities to mitigate __",
+  "According to Freedom on the Net, this is the 10th consecutive year of worsening __",
+  "My digital security trainings now only cover __",
+  "__ resolved internet freedom issues in my country",
+  "Peer to peer __ for marginalized groups",
+  "The only source of pure tech funding is from __"
   "A venture capital styled approach for __",
-  "Mesh-enabled __",
-  "According to Freedom on the Net, this is the 7th consecutive year of decreasing __",
-  "My digital security trainings now only cover __"
+  "__ - definitely the best way to get a cost extension",
+  "__ enabled __ is big with donors this year",
+  "overworked, underpaid, and looking for __",
+  "__ in a suitcase"
 );
 
 var arrayifwhitecard = new Array(
-  "Meetings at Casey's Coffee",
-  "internet freedom grant proposals",
-  "fragile states",
-  "cyber-unfree countries",
+  "cyber-nefarious countries",
+  "zero-rated services accidentally providing open proxies",
   "rejecting government funding in favor of corporate funding",
   "1-page copypasta risk assessments",
-  "vulnerable populations",
-  "freedom of expression",
-  "diaspora communities",
-  "Internet shutdowns",
+  "circumventing censorship but not surveillance",
   "yet another digital security training/guide/curricula",
-  "confusion between a guide and a curricula",
-  "Glitter"
+  "the eternal confusion between a guide and a curricula",
+  "freedom of expression",
+  "Internet shutdowns",
+  "diaspora communities",
+  "vulnerable populations",
+  "Meetings at Casey's Coffee",
+  "internet freedom grant proposals",
+  "taking the grant money and running",
+  "Unpaid internships at a social justice NGO",
+  "ADIDS",
+  "Trainings of trainers of trainers of trainers of trainers",
+  "Finding local high risk partners through public social media posts",
+  "accepting the risk on behalf of the beneficiary",
+  "a definitely winnable - but absolutely unsafe to implement grant proposal",
+  "Glitter",
+  "Actually doing the fucking work",
+  "Qubes",
+  "Post-it notes",
+  "Cuban p2p mesh twitter"
 );
 
 arraywow = new Array (
---
+++ bot/masto/bot.js
@@ -0,0 +1,39 @@
+import { login } from 'masto';
+
+const masto = await login({
+  url: "https://infosec.exchange",
+  accessToken: process.env.TOKEN,
+});
+
+
+import cards from '../../assets/dictionary.js';
+// var cards = require('../../assets/dictionary.js');
+
+var maxredcard = cards.arrayredcard.length - 1;
+var maxbluecard = cards.arraybluecard.length - 1;
+
+const randblue = Array(maxbluecard).fill().map((_, index) => index + 1);
+randblue.sort(() => Math.random() - 0.5);
+
+const randred = Array(maxredcard).fill().map((_, index) => index + 1);
+randred.sort(() => Math.random() - 0.5);
+
+var bluecard = cards.arraybluecard[randblue.slice(0,1)]
+
+var bluered = bluecard.replace("__", "<u id=\"redcard1\">" + cards.arrayredcard[randred.slice(0,1)] + "</u>");
+var bluered = bluecard.replace("__", "<u id=\"redcard1\">" + cards.arrayredcard[randred.slice(1,2)] + "</u>");
+var bluered = bluecard.replace("__", "<u id=\"redcard1\">" + cards.arrayredcard[randred.slice(2,3)] + "</u>");
+
+var statement = bluered.replace(/<[^>]+>/g, '_');
+
+
+const status = await masto.v1.statuses.create({
+  status: statement,
+  visibility: 'public',
+});
+
+
+console.time('timestamp');
+console.log('There are ' + maxbluecard + ' blue cards, and ' + maxredcard + ' red cards');
+console.log('Just generated a great ' + statement.length  + ' character-long idea.: ' + statement );
+console.log(status.url);
